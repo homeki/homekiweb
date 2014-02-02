b35087a39abc6826a5813fa6a00166eda617a552
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-02-02 23:34:01 +0100

    Adds error interceptor and directive for showing error message

---
+++ app/scripts/directives/hkMessage.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('app')
+  .directive('hkMessage', function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      template: '<div class="message-container"><div ng-show="show" ng-click="dismiss()" class="clickable alert alert-danger message" ng-bind="message"></div></div>',
+      link: function (scope) {
+        function setError(e, data) {
+          console.log(data);
+          var message = data.message === undefined ? data : data.message;
+          scope.show = true;
+          scope.message = message;
+        }
+
+        scope.dismiss = function () {
+          scope.show = false;
+          scope.message = '';
+        };
+
+        scope.$on('hkError', setError);
+      }
+    };
+  })
---
+++ app/scripts/services/message.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('app')
+  .factory('errorInterceptor', function ($q, $rootScope) {
+    return {
+      responseError: function (response) {
+        if (response.status === 0) {
+          $rootScope.$broadcast('hkError', 'Failed to connect to Homeki Server.');
+        } else if (response.status === 400 || response.status == 500) {
+          $rootScope.$broadcast('hkError', response.data);
+        } else {
+          $rootScope.$broadcast('hkError', 'An unknown error occured, received HTTP status ' + response.status + ' from server.');
+        }
+
+        return $q.reject(response);
+      }
+    };
+  })
+  .config(function ($httpProvider) {
+    $httpProvider.interceptors.push('errorInterceptor');
+  });
