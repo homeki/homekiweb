932b1cfa06d887f44e03888e9e47dd56654290c6
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-02-09 22:55:56 +0100

    Adds new DeviceChannel API resource (for setting channel values/reading available channels)

--- app/scripts/services/api.js
+++ app/scripts/services/api.js
@@ -5,6 +5,7 @@ angular.module('app')
     var DEV_SERVER = 'http://localhost:5000';
 
     return {
-      Device: $resource(DEV_SERVER + '/api/devices/:deviceId', { deviceId: '@deviceId'})
+      Device: $resource(DEV_SERVER + '/api/devices/:deviceId', { deviceId: '@deviceId'}),
+      DeviceChannel: $resource(DEV_SERVER + '/api/devices/:deviceId/channels/:channelId', { deviceId: '@deviceId', channelId: '@channelId' })
     };
   });
