22719f9a006922c84b071c0024b8890e04a30aee
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-03-02 11:44:16 +0100

    Disables DEV_SERVER in apiService

--- app/scripts/services/api.js
+++ app/scripts/services/api.js
@@ -2,7 +2,8 @@
 
 angular.module('app')
   .factory('api', function ($resource) {
-    var DEV_SERVER = 'http://192.168.1.79:5000';
+    //var DEV_SERVER = 'http://192.168.1.79:5000';
+    var DEV_SERVER = '';
 
     return {
       Device: $resource(DEV_SERVER + '/api/devices/:deviceId', { deviceId: '@deviceId' }),
