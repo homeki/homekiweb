d6945c2cc286c4fbc8d01aaf0987aebff1ae0e5b
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-11-17 00:36:02 +0100

    Add first circle test and deploy script

---
+++ script/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script deploys to S3 debian repository.
+
+set -e
+
+DEB_CODENAME=$1
+
+grunt build
+
+echo $GPG_PRIVATE_KEY | base64 --decode > homeki-private-gpg.asc
+set +e
+gpg --import homeki-private-gpg.asc
+rm homeki-private-gpg.asc
+set -e
+mv build/dist/homeki_*_all.deb build/dist/homeki_${DEB_CODENAME}.deb
+#bundle exec deb-s3 upload --endpoint s3-eu-west-1.amazonaws.com --sign $GPG_KEY_ID --access-key-id=$S3_ACCESS_KEY --secret-access-key=$S3_ACCESS_SECRET --codename $DEB_CODENAME --prefix packages --bucket repository.homeki.com ../build/dist/homeki_${DEB_CODENAME}.deb
