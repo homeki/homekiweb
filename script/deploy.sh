aa86035e820e4c903752dd993a089190e595c470
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-11-18 00:32:31 +0100

    cd to bundle dir when deploying

--- script/deploy.sh
+++ script/deploy.sh
@@ -12,4 +12,6 @@ gpg --import homeki-private-gpg.asc
 rm homeki-private-gpg.asc
 set -e
 mv tmp/homeki-web_*_all.deb tmp/homeki-web_${DEB_CODENAME}.deb
+pushd script > /dev/null
 bundle exec deb-s3 upload --endpoint s3-eu-west-1.amazonaws.com --sign $GPG_KEY_ID --access-key-id=$S3_ACCESS_KEY --secret-access-key=$S3_ACCESS_SECRET --codename $DEB_CODENAME --prefix packages --bucket repository.homeki.com tmp/homeki-web_${DEB_CODENAME}.deb
+popd > /dev/null
